e64e2748056d7f8375f0d885c786b7c900db24e7
mdhadiul0011 <mdhadiul0011@gmail.com>
2024-10-06 10:39:55 +0600

    new add

--- src/component/Homepart/hero.js
+++ src/component/Homepart/hero.js
@@ -9,13 +9,11 @@ function HeroPart() {
               <div className="hc-hero-content">
                 <div className="hc-hero-content-title">
                       <h2 className="title">Take best quality treatment for 
-                      <div  className="animate_text">
                       <span className=' text-primarycolor text-[60px] font-bold '>Crutches</span> 
                       <span className=' text-primarycolor text-[60px] font-bold '>Cardiology</span> 
                       <span className=' text-primarycolor text-[60px] font-bold '>Dentist</span> 
                       <span className=' text-primarycolor text-[60px] font-bold '>Neurology</span> 
                       <span className=' text-primarycolor text-[60px] font-bold '>Crutches</span> 
-                    </div>
                   </h2>
                   <p className='hero_sub_title'>The art of medicine consists in amusing the patient while nature cures the disease.
                   Treatment without prevention is simply unsustainable.</p>
