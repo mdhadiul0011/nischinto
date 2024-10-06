4dc63b3c3718f06c53376f06e4e88a506ad9aa76
mdhadiul0011 <mdhadiul0011@gmail.com>
2024-10-06 10:34:28 +0600

    new add

--- src/component/Homepart/hero.js
+++ src/component/Homepart/hero.js
@@ -1,29 +1,35 @@
 import React from 'react'
-import Container from '../Container'
 
 function HeroPart() {
   return (
     <section id='hero_part'>
-      <Container>
-        <div className='hero_content'>
-          <div className='hero_text'>
-            <h2 className="top_text">Take best quality treatment for 
-                <div  className="animate_text">
-                <span className=' text-primarycolor text-[60px] font-bold '>Crutches</span> 
-                <span className=' text-primarycolor text-[60px] font-bold '>Cardiology</span> 
-                <span className=' text-primarycolor text-[60px] font-bold '>Dentist</span> 
-                <span className=' text-primarycolor text-[60px] font-bold '>Neurology</span> 
-                <span className=' text-primarycolor text-[60px] font-bold '>Crutches</span> 
+        <div className="container">
+          <div className='row'>
+            <div className="col-lg-8 col-md-12">
+              <div className="hc-hero-content">
+                <div className="hc-hero-content-title">
+                      <h2 className="title">Take best quality treatment for 
+                      <div  className="animate_text">
+                      <span className=' text-primarycolor text-[60px] font-bold '>Crutches</span> 
+                      <span className=' text-primarycolor text-[60px] font-bold '>Cardiology</span> 
+                      <span className=' text-primarycolor text-[60px] font-bold '>Dentist</span> 
+                      <span className=' text-primarycolor text-[60px] font-bold '>Neurology</span> 
+                      <span className=' text-primarycolor text-[60px] font-bold '>Crutches</span> 
+                    </div>
+                  </h2>
+                  <p className='hero_sub_title'>The art of medicine consists in amusing the patient while nature cures the disease.
+                  Treatment without prevention is simply unsustainable.</p>
+                  <button className="hc-hero-btn">Appointment</button>
+                </div>
               </div>
-            </h2>
-            <p className='hero_sub_title'>The art of medicine consists in amusing the patient while nature cures the disease.
-            Treatment without prevention is simply unsustainable.</p>
+            </div>
+            <div className="col-lg-4">
+              <div className="hc-hero-thumb">
 
-            <button className="hero_btn">Appointment</button>
+              </div>
+            </div>
           </div>
-          <div className="hero_doc_img"></div>
         </div>
-      </Container>
     </section>
   )
 }
